b8f0cd5e63b5b76df7f08233825298d174c1c8d4
Jason Swartz <dev@bks2.com>
2013-07-20 23:34:05 -0700

    first

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ svss2013
 ========
 
 Silicon Valley Scala Symposium 2013 Site
+
+### Notes
+
+* use Sass to generate main.css from main.scss: `sass --watch main.scss:main.css`
---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+
+// Configure Google Maps
+
+function initialize() {
+  
+  var intel = new google.maps.LatLng(37.384672, -121.96383);
+
+  var mapOptions = { 
+    scrollwheel: false, 
+    zoom: 12, 
+    center: intel, 
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map_canvas = document.getElementById('map_canvas');
+
+  var map = new google.maps.Map( map_canvas, mapOptions );
+
+  var marker = new google.maps.Marker({
+      position: intel, map: map, title: 'Intel, 2191 Laurelwood, Santa Clara, CA'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+$(document).ready(function () {
+  var tooltipSettings = {animation: false, placement: 'bottom' };
+  $('.scheduleContents td a').tooltip( tooltipSettings );
+});
+
+
+
+
